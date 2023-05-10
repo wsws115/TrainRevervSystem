61a59de305702c0c3f7798a3ff09e7dbdbb4019b
Gi7A-09 <Gi7A-09@DESKTOP-OI19DI3>
2023-05-10 10:15:53 +0900

    기차역 번호 입력 코드

---
+++ TrainProject/src/train/SubwayCode.java
@@ -0,0 +1,58 @@
+package train;
+
+import java.util.HashMap;
+
+public class SubwayCode {
+	public static HashMap<String, String>sub_map = new HashMap<>();
+	public SubwayCode() {
+		sub_map.put("NAT020040","상봉");
+		sub_map.put("NAT130036","서빙고");
+		sub_map.put("NAT130070","옥수");
+		sub_map.put("NAT130104","왕십리");
+		sub_map.put("NAT130126","청량리");
+		sub_map.put("NAT130182","광운대");
+		sub_map.put("NAT010000","서울");
+		sub_map.put("NAT010032","용산");
+		sub_map.put("NAT010058","노량진");
+		sub_map.put("NAT010091","영등포");
+		sub_map.put("NAT010106","신도림");
+		sub_map.put("NATH30000","수서");
+		sub_map.put("NAT011403","부강");
+		sub_map.put("NAT011298","조치원");
+		sub_map.put("NAT011079","소정리");
+		sub_map.put("NAT011154","전의");
+		sub_map.put("NAT014244","화명");
+		sub_map.put("NAT014281","구포");
+		sub_map.put("NAT014331","사상");
+		sub_map.put("NAT014445","부산");
+		sub_map.put("NAT750046","부전");
+		sub_map.put("NAT750106","동래");
+		sub_map.put("NAT750161","센텀");
+		sub_map.put("NAT750189","신해운대");
+		sub_map.put("NAT750254","송정");
+		sub_map.put("NAT750329","기장");
+		sub_map.put("NAT750412","좌천");
+		sub_map.put("NAT013239","대구");
+		sub_map.put("NAT013271","동대구");
+		sub_map.put("NAT013189","서대구");
+		sub_map.put("NAT060231","주안");
+		sub_map.put("NATC30058","인천공항T2");
+		sub_map.put("NATC10325","검암");
+		sub_map.put("NATC10580","인천공항T1");
+		sub_map.put("NAT031857","광주송정");
+		sub_map.put("NAT882904","효천");
+		sub_map.put("NAT882936","서광주");
+		sub_map.put("NAT883012","광주");
+		sub_map.put("NAT883086","극락강");
+		sub_map.put("NAT011668","대전");
+		sub_map.put("NAT030057","서대전");
+		sub_map.put("NAT030173","흑석리");
+		sub_map.put("NAT011524","신탄진");
+		sub_map.put("NAT750781","북울산");
+		sub_map.put("NATH13717","울산(통도사)");
+		sub_map.put("NAT750560","남창");
+		sub_map.put("NAT750653","덕하");
+		sub_map.put("NAT750726","태화강");
+		sub_map.put("NAT750760","효문");
+	}
+}
